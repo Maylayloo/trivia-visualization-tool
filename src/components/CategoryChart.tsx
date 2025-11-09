aedbc1750cf42d0152f8a93057838943500b819c
Maylayloo <milosz.l-24@wp.pl>
2025-11-09 15:16:31 +0100

    Scaled Y axis

--- src/components/CategoryChart.tsx
+++ src/components/CategoryChart.tsx
@@ -7,7 +7,8 @@ interface CategoryBarProps {
 }
 
 const CategoryChart = ({categories, selectedCategory}: CategoryBarProps) => {
-
+    const maxCount = Math.max(...categories.map(c => c.count));
+    const yMax = Math.max(1, Math.ceil(maxCount * 1.2));
     const data = selectedCategory ? categories.filter((cat: Category) => cat.name === selectedCategory.name ) : categories
 
     return (
@@ -15,7 +16,7 @@ const CategoryChart = ({categories, selectedCategory}: CategoryBarProps) => {
             <BarChart data={data}>
                 <CartesianGrid strokeDasharray="3 3"/>
                 <XAxis dataKey="name"/>
-                <YAxis/>
+                <YAxis domain={[0, yMax]}/>
                 <Tooltip/>
                 <Bar dataKey="count" radius={[8, 8, 0, 0]} maxBarSize={90}>
                     {data.map((entry, index) => (
