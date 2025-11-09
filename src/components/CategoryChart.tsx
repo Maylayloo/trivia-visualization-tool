ac632530c2a1149c66a94619a409e4647b83654a
Maylayloo <milosz.l-24@wp.pl>
2025-11-09 04:55:49 +0100

    Category Chart

---
+++ src/components/CategoryChart.tsx
@@ -0,0 +1,30 @@
+import {BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, Cell, CartesianGrid} from "recharts";
+import type {Category} from "../App";
+
+interface CategoryBarProps {
+    categories: Category[];
+    selectedCategory: Category | null;
+}
+
+const CategoryChart = ({categories, selectedCategory}: CategoryBarProps) => {
+
+    const data = selectedCategory ? categories.filter((cat: Category) => cat.name === selectedCategory.name ) : categories
+
+    return (
+        <ResponsiveContainer width="100%" height={300}>
+            <BarChart data={data} className="focus:outline-none">
+                <CartesianGrid strokeDasharray="3 3"/>
+                <XAxis dataKey="name"/>
+                <YAxis/>
+                <Tooltip/>
+                <Bar dataKey="count" radius={[8, 8, 0, 0]}>
+                    {data.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={entry.color}/>
+                    ))}
+                </Bar>
+            </BarChart>
+        </ResponsiveContainer>
+    );
+};
+
+export default CategoryChart;
