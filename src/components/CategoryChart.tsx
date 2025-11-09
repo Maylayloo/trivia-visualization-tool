2a5a919a980cadf7daee4e3fc59ad0559cf685cc
Maylayloo <milosz.l-24@wp.pl>
2025-11-09 07:05:40 +0100

    Added maxBarSize

--- src/components/CategoryChart.tsx
+++ src/components/CategoryChart.tsx
@@ -11,13 +11,13 @@ const CategoryChart = ({categories, selectedCategory}: CategoryBarProps) => {
     const data = selectedCategory ? categories.filter((cat: Category) => cat.name === selectedCategory.name ) : categories
 
     return (
-        <ResponsiveContainer width="100%" height={300}>
-            <BarChart data={data} className="focus:outline-none">
+        <ResponsiveContainer width="100%" height={300} >
+            <BarChart data={data}>
                 <CartesianGrid strokeDasharray="3 3"/>
                 <XAxis dataKey="name"/>
                 <YAxis/>
                 <Tooltip/>
-                <Bar dataKey="count" radius={[8, 8, 0, 0]}>
+                <Bar dataKey="count" radius={[8, 8, 0, 0]} maxBarSize={90}>
                     {data.map((entry, index) => (
                         <Cell key={`cell-${index}`} fill={entry.color}/>
                     ))}
